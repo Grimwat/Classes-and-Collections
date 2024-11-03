045b66c8792a093bdf74e6faef2cade982bd1f95
Thomas <Thomas.watson555@Gmail.com>
2024-11-04 06:02:40 +1300

    Task 1

---
+++ app/src/main/java/com/example/practiceclasssandcollections/ui/Task1.kt
@@ -0,0 +1,12 @@
+package com.example.practiceclasssandcollections.ui
+
+data class Event
+    (
+    val title: String,
+    val description: String? = null,
+    val dayPart: String,
+    val durationInMinutes: Int,
+)
+
+
+
